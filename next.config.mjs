0513f93e6301af6993c36065ea1f133779585757
svictor <victorshayo677@gmail.com>
2025-07-04 20:13:16 +0300

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/Tamasha' : '',
   images: {
     unoptimized: true,
   },
